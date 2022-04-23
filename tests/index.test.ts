67a6c4a64e6efdc001d72d7d51bb68f20872b503
mvietnhi <mvietnhi@gmail.com>
2022-04-23 17:50:44 +0700

    feat(add_new_feature): update test
    + update test example

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,27 +1,27 @@
-// const { createServer } = require("http");
-// const { Server } = require("socket.io");
-// const Client = require("socket.io-client");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const Client = require("socket.io-client");
 
-// describe("my awesome project", () => {
-//   let clientSocket;
+describe("my awesome project", () => {
+  let clientSocket;
 
-//   // beforeAll((done) => {
-//     // done();
-//   // });
+  // beforeAll((done) => {
+    // done();
+  // });
 
-//   afterAll(() => {
-//     clientSocket.close();
-//   });
+  afterAll(() => {
+    clientSocket.close();
+  });
 
-//   it("test connection", (done) => {
-//     clientSocket = new Client('https://au-chat-server.herokuapp.com');
-//     clientSocket.emit("newMessage", "t1", "t2", "t3" );
+  it("test connection", (done) => {
+    clientSocket = new Client('http://localhost:3000');
+    clientSocket.emit("sendMessage", {'message': 'hiiiiii222222222222', 'userId': '6197ad0f561079889c705990', 'toUserId': '6170269e7417e20b9732dd28'});
 
-//     clientSocket.on("messageCreated", (arg) => {
-//       console.log("data",arg);
-//       expect(arg).toMatchObject({messageBody: "t1",userId: "t2",channelId: "t3"});
-//       done();
-//     });
-//   }, 10000);
+    clientSocket.on("messageCreated", (arg) => {
+      console.log("data",arg);
+      expect(arg).toMatchObject({messageBody: "hiiiiii222222222222", userId: "6197ad0f561079889c705990"});
+      done();
+    });
+  }, 10000);
 
-// });
+});
