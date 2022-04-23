221d491ec8e22331cc8d750318b948fb64f888d8
mvietnhi <mvietnhi@gmail.com>
2022-04-23 19:19:16 +0700

    feat(test_rebase): test rebase onto
    + test rebase onto

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,7 +6,7 @@ describe("my awesome project", () => {
   let clientSocket;
 
   // beforeAll((done) => {
-    // done();
+  // done();
   // });
 
   afterAll(() => {
@@ -14,14 +14,20 @@ describe("my awesome project", () => {
   });
 
   it("test connection", (done) => {
-    clientSocket = new Client('http://localhost:3000');
-    clientSocket.emit("sendMessage", {'message': 'hiiiiii222222222222', 'userId': '6197ad0f561079889c705990', 'toUserId': '6170269e7417e20b9732dd28'});
+    clientSocket = new Client("http://localhost:3000");
+    clientSocket.emit("sendMessage", {
+      message: "hiiiiii222222222222",
+      userId: "6197ad0f561079889c705990",
+      toUserId: "6170269e7417e20b9732dd28",
+    });
 
     clientSocket.on("messageCreated", (arg) => {
-      console.log("data",arg);
-      expect(arg).toMatchObject({messageBody: "hiiiiii222222222222", userId: "6197ad0f561079889c705990"});
+      console.log("data", arg);
+      expect(arg).toMatchObject({
+        messageBody: "hiiiiii222222222222",
+        userId: "6197ad0f561079889c705990",
+      });
       done();
     });
   }, 10000);
-
 });
