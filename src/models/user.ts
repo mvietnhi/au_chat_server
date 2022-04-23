122aa166babb8619c743585dfb8d13b38a0832d9
mvietnhi <mvietnhi@gmail.com>
2022-04-23 18:34:08 +0700

    feat(test_rebase): rebase from login 4
    + test rebase 4 from login (brach old)

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose';
-const bcrypt = require('bcryptjs');
+import mongoose from "mongoose";
+const bcrypt = require("bcryptjs");
 mongoose.Promise = global.Promise;
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    email: {
-        type: String,
-        required: true
-      },
-    password: {
-        type: String,
-        required: true
-      },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+  name: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', async function(next) {
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
-  });
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
 
 export default mongoose.model("User", userSchema);
