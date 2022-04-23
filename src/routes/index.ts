8d459d4bb49e72f319ab7e9e9e663a61d8cda84a
mvietnhi <mvietnhi@gmail.com>
2022-04-23 18:08:49 +0700

    feat(test_rebase): rebase 1
    + test rebase 1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import user from '../controllers/user.controller';
-import chat from '../controllers/chat.controller';
-import authHandler from '../middlewares/authHandler';
+import express from "express";
+import user from "../controllers/user.controller";
+import chat from "../controllers/chat.controller";
+import authHandler from "../middlewares/authHandler";
 
 let router = express();
 
-router.use('/user', user());
-router.use('/chat', authHandler, chat());
+router.use("/user", user());
+router.use("/chat", authHandler, chat());
 
 export default router;
