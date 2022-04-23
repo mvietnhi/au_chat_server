7949085af1013d29879dff68120c9c2178bb863e
mvietnhi <mvietnhi@gmail.com>
2022-04-23 18:32:12 +0700

    feat(test_rebase): rebase from login
    + test rebase 1 from login (brach old)

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,26 +1,26 @@
-import mongoose from 'mongoose';
-import { Router } from 'express';
-import bodyParser from 'body-parser';
-import User from '../models/user';
+import mongoose from "mongoose";
+import { Router } from "express";
+import bodyParser from "body-parser";
+import User from "../models/user";
 
 export default () => {
-    let api = Router();
-    console.log('Router');
-    // '/v1/user/' - Read
-    api.post('/register', (req, res) => {
-        console.log('Router/register', req.body);
-        let newUser = new User();
-        newUser.name = req.body.name;
-        newUser.email = req.body.email;
-        newUser.password = req.body.password;
+  let api = Router();
+  console.log("Router");
+  // '/v1/user/' - Read
+  api.post("/register", (req, res) => {
+    console.log("Router/register", req.body);
+    let newUser = new User();
+    newUser.name = req.body.name;
+    newUser.email = req.body.email;
+    newUser.password = req.body.password;
 
-        newUser.save(err => {
-            if (err) {
-              res.status(500).json({ message: err });
-            }
-            res.status(200).json({ message: 'User register' });
-          });
+    newUser.save((err) => {
+      if (err) {
+        res.status(500).json({ message: err });
+      }
+      res.status(200).json({ message: "User register" });
     });
+  });
 
-    return api;
-}
+  return api;
+};
